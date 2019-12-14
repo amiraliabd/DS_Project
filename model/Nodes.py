faf29a23c4a2685c61f07306986366d80f38838a
amirali <email@example.com>
2019-12-14 16:55:38 +0330

    NOde subclasses except from amd in list

--- model/Nodes.py
+++ model/Nodes.py
@@ -10,3 +10,45 @@ class Node:
     # list of information
     about = []
 
+class Person(Node):
+    def __init__(self, a:dict):
+        for k in a.keys():
+            self.key=k
+            for value in a.get(k):
+                self.about.append(value)
+        pass#from and to must be complete
+
+
+class banck(Node):
+    def __init__(self, a: dict):
+        for k in a.keys():
+            self.key = k
+            for value in a.get(k):
+                self.about.append(value)
+        pass  # from and to must be complete
+
+class home(Node):
+    def __init__(self, a:dict):
+        for k in a.keys():
+            self.key=k
+            for value in a.get(k):
+                self.about.append(value)
+        pass#from and to must be complete
+
+class car(Node):
+    def __init__(self, a:dict):
+        for k in a.keys():
+            self.key=k
+            for value in a.get(k):
+                self.about.append(value)
+        pass#from and to must be complete
+
+class phone(Node):
+    def __init__(self, a:dict):
+        for k in a.keys():
+            self.key=k
+            for value in a.get(k):
+                self.about.append(value)
+        pass#from and to must be complete
+
+
