8953406b532c926d7f17f0e0070eca1613645949
amirali <email@example.com>
2019-12-31 10:45:31 +0330

    Node classes is done

--- model/Nodes.py
+++ model/Nodes.py
@@ -1,25 +1,42 @@
 from abc import ABC,abstractclassmethod
+
+
 class Node:
     @abstractclassmethod
     def __init__(self):
         pass
-    #edge witch is comming in or going out
-    IN=[]
-    OUT=[]
     key = None
     # list of information
     about = []
 
+
 class Person(Node):
+    """
+    key:
+        person ID
+    about list contains :
+        first name
+        last name
+        birthday
+        birth place
+        place of work
+    """
     def __init__(self, a:dict):
         for k in a.keys():
             self.key=k
             for value in a.get(k):
                 self.about.append(value)
-        pass#from and to must be complete
 
 
-class banck(Node):
+class Bank(Node):
+    """
+    key:
+        Bank account number
+    about list contains :
+        owner ID
+        bank name
+        number
+    """
     def __init__(self, a: dict):
         for k in a.keys():
             self.key = k
@@ -27,7 +44,18 @@ class banck(Node):
                 self.about.append(value)
         pass  # from and to must be complete
 
-class home(Node):
+
+class Home(Node):
+    """
+    key:
+        postal code
+    about list contains :
+        owner ID
+        cost
+        area
+        address
+    """
+
     def __init__(self, a:dict):
         for k in a.keys():
             self.key=k
@@ -35,7 +63,14 @@ class home(Node):
                 self.about.append(value)
         pass#from and to must be complete
 
-class car(Node):
+class Car(Node):
+    """
+    key:
+        plate number
+    about list contains :
+    owner ID
+    model
+    """
     def __init__(self, a:dict):
         for k in a.keys():
             self.key=k
@@ -43,7 +78,15 @@ class car(Node):
                 self.about.append(value)
         pass#from and to must be complete
 
-class phone(Node):
+
+class Phone(Node):
+    """
+    key:
+        number
+    about list contains :
+        owner ID
+        operator name
+    """
     def __init__(self, a:dict):
         for k in a.keys():
             self.key=k
