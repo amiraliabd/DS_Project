c385480449fea2883bbd9bf392aab0309606c7f5
amirali <email@example.com>
2020-02-19 16:33:12 +0330

    cleaning the code

--- Project/Project.py
+++ Project/Project.py
@@ -3,19 +3,20 @@ from prettytable import from_csv
 from tabulate import tabulate
 import networkx as nx
 from itertools import combinations
+
 '''
 using pandas lib for making dataframes for nodes and edges
 
 '''
-accountDF=None
-callDF=None
-carDF=None
-homeDF=None
-ownershipDF=None
-peopleDF=None
-phoneDF=None
-relationshipDF=None
-transactionDF=None
+accountDF = None
+callDF = None
+carDF = None
+homeDF = None
+ownershipDF = None
+peopleDF = None
+phoneDF = None
+relationshipDF = None
+transactionDF = None
 relationDF = pd.read_csv("sample data/relationships.csv")
 accountDF = pd.read_csv('sample data/accounts.csv')
 callDF = pd.read_csv('sample data/calls.csv')
@@ -26,13 +27,18 @@ peopleDF = pd.read_csv('sample data/people.csv')
 phoneDF = pd.read_csv('sample data/phones.csv')
 relationshipDF = pd.read_csv('sample data/relationships.csv')
 transactionDF = pd.read_csv('sample data/transactions.csv')
+
+
 # faze 1:---------------------------------------------------------------------------------------------------------------
 def Tables(address):
     filepath = open(address, "r")
     table = from_csv(filepath)
     filepath.close()
     return table
-print('faze 1:---------------------------------------------------------------------------------------------------------------')
+
+
+print(
+    'faze 1:---------------------------------------------------------------------------------------------------------------')
 print("accounts table:")
 print(Tables("sample data/accounts.csv"))
 print("phones table:")
@@ -44,23 +50,26 @@ print(Tables("sample data/homes.csv"))
 print("cars table:")
 print(Tables("sample data/cars.csv"))
 # faze 2:---------------------------------------------------------------------------------------------------------------
-print('faze 2:---------------------------------------------------------------------------------------------------------------')
-df=ownershipDF.loc[(ownershipDF['date']>='2018-00-00') & (ownershipDF['date']<='2020-00-00')]
-ls=[df['from'].iloc[i] for i in range(len(df))]
-#print(tabulate(df,tablefmt='psql',headers='keys'))
-result0=peopleDF.loc[(peopleDF['ssn'].isin(ls)) & ((peopleDF['work']=='گمرک') | (peopleDF['work'].str.contains('بندر') ) )]
-#print(tabulate(result0, tablefmt='psql', headers='keys'))
-ls=[result0['ssn'].iloc[i] for i in range(len(result0))]
-rel=['PARENT', 'OFFSPRING', 'SPOUSE', 'SIBLING']
-result1=relationshipDF.loc[( (relationshipDF['from'].isin(ls)) |(relationshipDF['to'].isin(ls))) ]
-#print(tabulate(result1,tablefmt='psql',headers='keys'))
-result2=result1.loc[ (result1['relation'].isin(rel)) ]
-#print(tabulate(final_result,tablefmt='psql',headers='keys'))
-ls=[result2['from'].iloc[i] for i in range(len(result2))]
-final_result=peopleDF.loc[ (peopleDF['ssn'].isin(ls))]
+print(
+    'faze 2:---------------------------------------------------------------------------------------------------------------')
+df = ownershipDF.loc[(ownershipDF['date'] >= '2018-00-00') & (ownershipDF['date'] <= '2020-00-00')]
+ls = [df['from'].iloc[i] for i in range(len(df))]
+# print(tabulate(df,tablefmt='psql',headers='keys'))
+result0 = peopleDF.loc[
+    (peopleDF['ssn'].isin(ls)) & ((peopleDF['work'] == 'گمرک') | (peopleDF['work'].str.contains('بندر')))]
+# print(tabulate(result0, tablefmt='psql', headers='keys'))
+ls = [result0['ssn'].iloc[i] for i in range(len(result0))]
+rel = ['PARENT', 'OFFSPRING', 'SPOUSE', 'SIBLING']
+result1 = relationshipDF.loc[((relationshipDF['from'].isin(ls)) | (relationshipDF['to'].isin(ls)))]
+# print(tabulate(result1,tablefmt='psql',headers='keys'))
+result2 = result1.loc[(result1['relation'].isin(rel))]
+# print(tabulate(final_result,tablefmt='psql',headers='keys'))
+ls = [result2['from'].iloc[i] for i in range(len(result2))]
+final_result = peopleDF.loc[(peopleDF['ssn'].isin(ls))]
 print(tabulate(final_result, tablefmt='psql', headers='keys'))
 # faze 3:---------------------------------------------------------------------------------------------------------------
-print('faze 3:---------------------------------------------------------------------------------------------------------------')
+print(
+    'faze 3:---------------------------------------------------------------------------------------------------------------')
 
 
 def bidirectional_pred_succ(G, source, target):
@@ -117,8 +126,9 @@ def bidirectional_SP(G, source, target):
     return len(path)
 
 
-
 # making graph of transactions
+
+
 TransactionsGraph = nx.Graph()
 transaction = zip(transactionDF["from"], transactionDF["to"])
 TransactionsGraph.add_edges_from(transaction)
@@ -129,8 +139,8 @@ smuglers = peopleDF.loc[(peopleDF['work'] == 'قاچاقچی')]
 doubtssn = [doubt['ssn'].iloc[i] for i in range(len(doubt))]
 smuglersssn = [smuglers['ssn'].iloc[j] for j in range(len(smuglers))]
 doubledoubt = []
-smuglersaccount = accountDF.loc[ (accountDF['ssn'].isin(smuglersssn))]
-doubtsaccount = accountDF.loc[ (accountDF['ssn'].isin(doubtssn))]
+smuglersaccount = accountDF.loc[(accountDF['ssn'].isin(smuglersssn))]
+doubtsaccount = accountDF.loc[(accountDF['ssn'].isin(doubtssn))]
 fromsmugle = smuglersaccount["account_id"]
 todoubt = doubtsaccount["account_id"]
 for sid in fromsmugle:
@@ -139,22 +149,33 @@ for sid in fromsmugle:
             if nx.has_path(TransactionsGraph, sid, did):
                 if bidirectional_SP(TransactionsGraph, sid, did) <= 5:
                     doubledoubt.append(did)
-doubt_frame=accountDF[accountDF['account_id'].isin(doubledoubt)]
-ls=[doubt_frame['ssn'].iloc[i] for i in range(len(doubt_frame))]
-doubt_frame=peopleDF[ peopleDF['ssn'].isin(ls) ]
+doubt_frame = accountDF[accountDF['account_id'].isin(doubledoubt)]
+ls = [doubt_frame['ssn'].iloc[i] for i in range(len(doubt_frame))]
+doubt_frame = peopleDF[peopleDF['ssn'].isin(ls)]
 print(tabulate(doubt_frame, tablefmt='psql', headers='keys'))
 # faze 4:---------------------------------------------------------------------------------------------------------------
-print('faze 4:---------------------------------------------------------------------------------------------------------------')
-doubtssn=[doubt_frame['ssn'].iloc[i] for i in range(len(doubt_frame))]
-doubtphonenumber=phoneDF[(phoneDF['ssn'].isin(doubtssn))]
-lst_doubtphonenumber=[doubtphonenumber['number'].iloc[j] for j in range(len(doubtphonenumber))]
-smuglerssn=[smuglers['ssn'].iloc[i] for i in range(len(smuglers))]
-smuglerphonenumber=phoneDF[(phoneDF['ssn'].isin(smuglerssn))]
-list_smuglerphonenumber=[smuglerphonenumber['number'].iloc[i] for i in range(len(smuglerphonenumber))]
-number_result=callDF[( (callDF['from'].isin(list_smuglerphonenumber)) & (callDF['to'].isin(lst_doubtphonenumber)) )]
-final_list=[number_result['to'].iloc[i] for i in range(len(number_result))]
-final_Frame=phoneDF[ (phoneDF['number'].isin(final_list)) ]
-final_list=[final_Frame['ssn'].iloc[i] for i in range(len(final_Frame))]
-final_Frame=peopleDF[ (peopleDF['ssn'].isin(final_list)) ]
-print(tabulate(final_Frame, tablefmt='psql', headers='keys'))
+print('faze 4:--------------------------------------------------------------------------------------------------------')
 
+doubtssn = [doubt_frame['ssn'].iloc[i] for i in range(len(doubt_frame))]
+
+doubtphonenumber = phoneDF[(phoneDF['ssn'].isin(doubtssn))]
+
+lst_doubtphonenumber = [doubtphonenumber['number'].iloc[j] for j in range(len(doubtphonenumber))]
+
+smuglerssn = [smuglers['ssn'].iloc[i] for i in range(len(smuglers))]
+
+smuglerphonenumber = phoneDF[(phoneDF['ssn'].isin(smuglerssn))]
+
+list_smuglerphonenumber = [smuglerphonenumber['number'].iloc[i] for i in range(len(smuglerphonenumber))]
+
+number_result = callDF[((callDF['from'].isin(list_smuglerphonenumber)) & (callDF['to'].isin(lst_doubtphonenumber)))]
+
+final_list = [number_result['to'].iloc[i] for i in range(len(number_result))]
+
+final_Frame = phoneDF[(phoneDF['number'].isin(final_list))]
+
+final_list = [final_Frame['ssn'].iloc[i] for i in range(len(final_Frame))]
+
+final_Frame = peopleDF[(peopleDF['ssn'].isin(final_list))]
+
+print(tabulate(final_Frame, tablefmt='psql', headers='keys'))
